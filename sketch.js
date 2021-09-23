e4a0d6c69b7e35b5238e84e312f7c56dc871f2f2
treefalldown <64744535+treefalldown@users.noreply.github.com>
2021-09-23 09:22:47 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,314 @@
+// Benedikt Groß
+
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Mouse = Matter.Mouse;
+const MouseConstraint = Matter.MouseConstraint;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let ground;
+let wall01;
+let wall02;
+let bridge;
+let ball;
+
+let compositeConstruction;
+
+let circ01;
+let rect01;
+let circ02;
+let rect02;
+let circ03;
+let rect03;
+
+let constraint01;
+let constraint02;
+let constraint03;
+let constraint04;
+let constraint05;
+let constraint06;
+let constraint07;
+let constraint08;
+let constraint09;
+let constraint010;
+let constraint011;
+
+let wheelbase;
+let wheelAOffset;
+let wheelYOffest;
+
+let group1;
+
+let canvas;
+
+function setup() {
+  canvas = createCanvas(800, 800);
+
+  // create an engine
+  engine = Engine.create();
+
+
+
+  group1 = Body.nextGroup(true),
+  wheelBase = 20,
+  wheelAOffset = -width * 0.5 + wheelBase,
+  // wheelBOffset = width * 0.5 - wheelBase,
+  wheelYOffset = 0;
+
+  compositeConstruction = Composite.create({ label: 'Car'});
+
+
+  circ01 = Bodies.circle(400, 600, 40, {
+    collisionFilter: {
+      group: group1
+    },
+    density: 0.0002
+  });
+
+  rect01 = Bodies.rectangle(360, 300, 80, 300, {
+    collisionFilter: {
+      group: group1
+    },
+    density: 0.0002
+  });
+
+  circ02 = Bodies.circle(400, 600, 20, {
+    collisionFilter: {
+      group: group1
+    },
+    density: 0.0002
+  });
+
+  rect02 = Bodies.rectangle(397.5, 320, 5, 270, {
+    collisionFilter: {
+      group: group1
+    },
+    friction: 0.8
+  });
+
+  circ03 = Bodies.circle(400, 250, 75, {
+    // isStatic: true,
+    collisionFilter: {
+      group: group1
+    },
+    density: 0.0002
+  });
+
+  rect03 = Bodies.rectangle(430, 250, 60, 110, {
+    collisionFilter: {
+      group: group1
+    },
+    chamfer: {
+      radius:10
+    },
+    friction: 0.8
+  });
+
+
+  constraint01 = Constraint.create({
+    bodyA: circ01,
+    // pointA: offsetA,
+    pointA: { x: 0, y: 0 },
+    bodyB: rect01,
+    pointB: { x: 0, y: 150 },
+    stiffness: 0.01,
+    length: 0
+  });
+
+  constraint02 = Constraint.create({
+    bodyA: circ01,
+    // pointA: offsetA,
+    pointA: { x: 0, y: 0 },
+    bodyB: circ02,
+    pointB: { x: 0, y: 0 },
+    stiffness: 0.01,
+    length: 0
+  });
+
+  constraint03 = Constraint.create({
+    bodyA: circ03,
+    // pointA: offsetA,
+    pointA: { x: 0, y: 150 },
+    bodyB: rect01,
+    pointB: { x: 0, y: 0 },
+    stiffness: 0.01,
+    length: 0
+  });
+
+  constraint04 = Constraint.create({
+    bodyA: rect02,
+    // pointA: offsetA,
+    pointA: { x: 0, y: 150 },
+    bodyB: circ01,
+    pointB: { x: 0, y: -0 },
+    stiffness: 0.00001,
+    length: 0
+  });
+
+  constraint05 = Constraint.create({
+    bodyA: circ03,
+    // pointA: offsetA,
+    pointA: { x: 0, y: 0 },
+    bodyB: rect02,
+    pointB: { x: 0, y: 150 },
+    stiffness: 0.01,
+    length: 0
+  });
+
+
+  // Composite.add(compositeConstruction, [circ01, rect01, circ02, rect02, circ03, rect03, constraint01, constraint02, constraint03, constraint04, constraint05]);
+  Composite.add(compositeConstruction, [circ01, rect01, circ02, rect02, circ03, constraint01, constraint02, constraint03, constraint04, constraint05]);
+  // World.add(engine.world, [rect01, rect02, constraint01]);
+  // Composite.add(compositeConstruction, rect02);
+  // Composite.add(compositeConstruction, rect03);
+
+
+  // Composite.add(world, [rect01, rect02, rect03]);
+  // Composite.add(world, [rect])
+
+
+  World.add(engine.world, [compositeConstruction]);
+
+
+
+
+
+  // // add bridge
+  // const group = Body.nextGroup(true);
+  // const rects = Composites.stack(100, 200, 10, 1, 10, 10, function(x, y) {
+  //   return Bodies.rectangle(x, y, 50, 20, { collisionFilter: { group: group } });
+  // });
+  // bridge = Composites.chain(rects, 0.5, 0, -0.5, 0, {stiffness: 0.8, length: 2, render: {type: 'line'}});
+  // World.add(engine.world, [bridge]);
+  //
+  // // left and right fix point of bridge
+  // Composite.add(rects, Constraint.create({
+  //   pointA: {x: 100, y: 200},
+  //   bodyB: rects.bodies[0],
+  //   pointB: {x: -25, y: 0},
+  //   stiffness: 0.1
+  // }));
+  // Composite.add(rects, Constraint.create({
+  //   pointA: {x: 700, y: 200},
+  //   bodyB: rects.bodies[rects.bodies.length-1],
+  //   pointB: {x: +25, y: 0},
+  //   stiffness: 0.02
+  // }));
+  //
+  // // add ball
+  // ball = Bodies.circle(400, 0, 50);
+  // World.add(engine.world, [ball]);
+
+  // ground
+
+  wall01 = Bodies.rectangle(-250, 0, 500, height * 50, {isStatic: true});
+  wall02 = Bodies.rectangle(width+1000, 0, 1000, height * 50, {isStatic: true});
+
+  World.add(engine.world, [wall01, wall02])
+
+  ground = Bodies.rectangle(0, height + 50, 1810, 100, {isStatic: true});
+  World.add(engine.world, [ground]);
+
+  // setup mouse
+  const mouse = Mouse.create(canvas.elt);
+  const mouseParams = {
+    mouse: mouse,
+    constraint: { stiffness: 0.05 }
+  }
+  mouseConstraint = MouseConstraint.create(engine, mouseParams);
+  mouseConstraint.mouse.pixelRatio = pixelDensity();
+  World.add(engine.world, mouseConstraint);
+
+  // run the engine
+  Engine.run(engine);
+}
+
+function draw() {
+  background(255);
+
+  noStroke();
+  stroke('red');
+  noFill();
+  // drawVertices(ball.vertices);
+  // drawBodies(bridge.bodies);
+
+  fill(0);
+  drawVertices(circ01.vertices);
+  drawVertices(rect01.vertices);
+  fill(255);
+  drawVertices(circ02.vertices);
+  drawVertices(rect02.vertices);
+  fill(0);
+  drawVertices(circ03.vertices);
+  fill(255);
+  drawVertices(rect03.vertices);
+
+
+  stroke(128);
+  strokeWeight(2);
+  // drawConstraints(bridge.constraints);
+
+  noStroke();
+  fill(128);
+  drawVertices(ground.vertices);
+  drawVertices(wall01.vertices);
+  drawVertices(wall02.vertices);
+
+  drawMouse(mouseConstraint);
+}
+
+function drawConstraints(constraints) {
+  for (let i = 0; i < constraints.length; i++) {
+    drawConstraint(constraints[i]);
+  }
+}
+
+function drawBodies(bodies) {
+  for (let i = 0; i < bodies.length; i++) {
+    drawVertices(bodies[i].vertices);
+  }
+}
+
+function drawConstraint(constraint) {
+  const offsetA = constraint.pointA;
+  let posA = {x:0, y:0};
+  if (constraint.bodyA) {
+    posA = constraint.bodyA.position;
+  }
+  const offsetB = constraint.pointB;
+  let posB = {x:0, y:0};
+  if (constraint.bodyB) {
+    posB = constraint.bodyB.position;
+  }
+  line(
+    posA.x + offsetA.x,
+    posA.y + offsetA.y,
+    posB.x + offsetB.x,
+    posB.y + offsetB.y
+  );
+}
+
+function drawMouse(mouseConstraint) {
+  if (mouseConstraint.body) {
+    const pos = mouseConstraint.body.position;
+    const offset = mouseConstraint.constraint.pointB;
+    const m = mouseConstraint.mouse.position;
+    stroke(0, 255, 0);
+    strokeWeight(2);
+    line(pos.x + offset.x, pos.y + offset.y, m.x, m.y);
+  }
+}
+
+function drawVertices(vertices) {
+  beginShape();
+  for (let i = 0; i < vertices.length; i++) {
+    vertex(vertices[i].x, vertices[i].y);
+  }
+  endShape(CLOSE);
+}
